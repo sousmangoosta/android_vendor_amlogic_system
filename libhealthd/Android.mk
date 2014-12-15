82fcfc3b7389cfe1de0e5b66cff77c5d654d49d8
Chunjian Zheng <chunjian.zheng@amlogic.com>
2014-12-15 15:30:52 +0800

    add tablet board libhealthd
    Change-Id: Ief22483070dfac3ed1444f7330194e051c3cab4c

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -11,4 +11,11 @@ LOCAL_C_INCLUDES := system/core/healthd
 LOCAL_CFLAGS := -Werror
 include $(BUILD_STATIC_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := healthd_board_tablet_default.cpp
+LOCAL_MODULE := libhealthd.tablet
+LOCAL_C_INCLUDES := system/core/healthd
+LOCAL_CFLAGS := -Werror
+include $(BUILD_STATIC_LIBRARY)
+
 endif
