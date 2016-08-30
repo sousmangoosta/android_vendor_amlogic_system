cc84db0c1e001e32f3ad4214fafe00e34741462e
Tellen Yu <tellen.yu@amlogic.com>
2016-08-30 19:14:59 +0800

    modify for Nougat bringup
    Change-Id: I3fdd22e9a5cee567ba7e2c8f6fdfd9f33c2740b1

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -7,14 +7,16 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_mbox_default.cpp
 LOCAL_MODULE := libhealthd.mboxdefault
-LOCAL_C_INCLUDES := system/core/healthd
+LOCAL_C_INCLUDES := system/core/healthd \
+                    system/core/healthd/include/healthd
 LOCAL_CFLAGS := -Werror
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_tablet_default.cpp
 LOCAL_MODULE := libhealthd.tablet
-LOCAL_C_INCLUDES := system/core/healthd
+LOCAL_C_INCLUDES := system/core/healthd \
+                    system/core/healthd/include/healthd
 LOCAL_CFLAGS := -Werror
 include $(BUILD_STATIC_LIBRARY)
 
