c992cf862af307b6cd3ec785d582332d6fa944e8
pengcheng chen <pengcheng.chen@amlogic.com>
2017-11-21 17:08:22 +0800

    ge2dlib: add ge2d bad chip check tools [1/1]
    PD#155149
    
    add ge2d bad chip check tools
    
    Change-Id: Ifc9f883f26e269cdb62e51356fa7cb8c9a02214c
    Signed-off-by: pengcheng chen <pengcheng.chen@amlogic.com>

--- libge2d/Android.mk
+++ libge2d/Android.mk
@@ -75,3 +75,33 @@ LOCAL_PROPRIETARY_MODULE := true
 endif
 
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_PRELINK_MODULE:= false
+
+
+LOCAL_ARM_MODE := arm
+LOCAL_MODULE_TAGS := ge2d_chip_check
+LOCAL_SRC_FILES:= aml_ge2d.c IONmem.c ge2d_chip_check.c
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include \
+    $(LOCAL_PATH)/kernel-headers \
+	system/core/libion/include/ \
+	system/core/libion/kernel-headers \
+	system/core/include/ion
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	libge2d \
+	libion
+
+LOCAL_CFLAGS +=-g
+LOCAL_CPPFLAGS := -g
+
+LOCAL_MODULE := ge2d_chip_check
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
+include $(BUILD_EXECUTABLE)
